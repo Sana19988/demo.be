cfc3d0d9d5752622003881223f6028c243caa5f4
Sana19988 <tesica84@gmail.com>
2023-05-14 20:51:12 +0200

    Continuing Task

--- demo1.be/src/main/java/com/example/demo1/models/UserModel.java
+++ demo1.be/src/main/java/com/example/demo1/models/UserModel.java
@@ -1,7 +1,10 @@
 package com.example.demo1.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.NonNull;
 
 @Entity
 @Table(name = "users")
@@ -11,10 +14,14 @@ public class UserModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "first_name")
+    @NotBlank
     private String firstName;
     @Column(name = "last_name")
+    @NotBlank
     private String lastName;
     @Column(name = "email")
+    @Email
+    @NotBlank
     private String email;
 
 }
